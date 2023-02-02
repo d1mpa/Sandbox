bbca2946fb2d84f78facf68252bb6d690e2c2706
Dmitry Pavlov <dmitry.pavlov@complevo.de>
2023-02-02 14:51:36 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ChatGPT.Net;
+using ChatGPT.Net.DTO;
+using ChatGPT.Net.Enums;
+
+var chatGpt = new ChatGpt(new ChatGptConfig  
+{  
+    UseCache = true  
+});
+await chatGpt.WaitForReady();
+var chatGptClient = await chatGpt.CreateClient(new ChatGptClientConfig
+{
+    SessionToken = "",  
+    AccountType = AccountType.Free
+});
+var conversationId = "4d3016ad-8b53-4492-b24b-138bb45cb3cd";
+var response = await chatGptClient.Ask("What is the weather like today?", conversationId);
+Console.WriteLine(response);
+
+await chatGptClient.ResetConversation(conversationId);
+
+Console.WriteLine("Hello, World!");
